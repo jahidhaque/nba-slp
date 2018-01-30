775230663c02038c9237f5b55490ca1789f60906
greg <emegwalio@yahoo.com>
2018-01-30 08:57:23 +0000

    welcome email changes and design

--- app_server/controllers/authentication.js
+++ app_server/controllers/authentication.js
@@ -92,33 +92,26 @@ module.exports.signup = (req, res) => {
                                 </header><br/>
 
                                 <p>Dear ${req.body.firstName} ${req.body.lastName}, </p><br/>
-                                <p style="color: green">Thank you for registring with the NBA Section On Legal Practice</p>
+                                <p style="color: green">Thank you for registering with the NBA Section on Legal Practice</p>
 
                                 <p>
-                                    As the Chairman of the Section On Legal Practice, 
-                                    I wanted to reach out to you and welcome you to the Section. 
-                                    You're joining a Legal community of over 300,000 lawyers, and we're 
-                                    really excited to have you here.
+                                    On behalf of the Exco and Council Members I want  to welcome you to the Section. You're joining a Legal 
+                                    community of thousands of lawyers, and we are happy to have you here.
                                 </p>
 
-                                <p>Now that you're a part of our community, you can expect:</p>
-                                  <ul>              
-                                        <li>Join membership to any of the Section's Committee of your choice</li>
-                                        <li> And also note that you can join as many Committees as you can be committed to.</li>
-                                  </ul>
-                                 <p>These are subject upon satisfactory completion of your profile and payment of the Section dues</p>
-
-                                <p>The Section is currently made up of Fourteen Committees that promote the participation of 
-                                members of the Association towards achieving the objectives of the Section.</p>
-
-                                <p>Our Membership base is growing each and every day, but our goal has always 
-                                remained the same regardless of our size; we want to have a personal connection 
-                                with each and every member. If you have any questions regarding your Membership, 
-                                or even just want to chat, we're here to help.</p>
-
-                                <p>our web site is <a href="www.nba-slp.org">Nba-slp.org</a> </p><br/>
-                                <p>Well, I think that's it! So, on behalf of the entire Section On Legal 
-                                Practice team: welcome!</p><br>
+                                <p>The Section is currently made up of Fourteen Committees that promote the participation of members of the Nigerian
+                                 Bar Association towards achieving the objectives of the Section. Now that you're a part of our community, we encourage 
+                                 you to join any of the Section's committees, details are on our website.
+                                 </p>
+                                  
+                                 <p>Our Membership base is growing every day, but our goal has always remained the same regardless of our size, we want 
+                                 to have a personal connection with each and every member. If you have any questions regarding your Membership, please 
+                                 get in touch with us via our website
+                                 </p>
+                                                              
+                                <p>our web site is <a href="http.nba-slp.org">Nba-slp.org</a> </p><br/>
+                                
+                                <p>So, on behalf of the entire Section on Legal Practice you are welcome!</p><br>
 
                                 <p>Mrs Mia Essien SAN</p>
                                 <p><b>SECTION CHAIRMAN</b></p><br>
